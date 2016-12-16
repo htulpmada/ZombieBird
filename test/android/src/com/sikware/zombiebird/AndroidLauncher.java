167e83a8daea3eef1e1f2a273afcf22130aed80d
i am a robot <adampluth@yahoo.com>
2016-12-16 09:40:55 -0600

    initial setup

---
+++ test/android/src/com/sikware/zombiebird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sikware.zombiebird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sikware.zombiebird.ZBGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZBGame(), config);
+	}
+}
---
+++ test/html/src/com/sikware/zombiebird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sikware.zombiebird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sikware.zombiebird.ZBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ZBGame();
+        }
+}
